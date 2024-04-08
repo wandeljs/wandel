3b36979f5b6ada52acd01375b5c79c7365dcc691
Jan-Niklas Wortmann <jan-niklas.wortmann@jetbrains.com>
2024-04-08 10:20:48 -0500

    fix knip issues

--- vitest.shared.ts
+++ vitest.shared.ts
@@ -3,7 +3,12 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
 	test: {
 		clearMocks: true,
-		exclude: ["lib", "node_modules"],
+		exclude: ["node_modules"],
 		setupFiles: ["console-fail-test/setup"],
+		coverage: {
+			all: true,
+			include: ["packages"],
+			reporter: ["html", "lcov"],
+		},
 	},
 });
